a408a961a22eb2bfaa3d0860a0c9d1bf733693c1
Suraj Verma <surajverma@rentalcars.com>
2021-03-20 13:55:23 +0000

    add timer feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 const startButtonElement = document.getElementById("start-btn");
 const startGameDiv = document.getElementById("start-game");
+const timerSpanElement = document.getElementById("timer");
 const bodyElement = document.body;
 
 const gameWord = "hello";
+let timerValue = 3;
 
 const constructLetterBoxes = () => {
   // spread operator
@@ -20,6 +22,7 @@ const constructLetterBoxes = () => {
 const constructGameContainer = () => {
   const gameContainerDiv = document.createElement("div");
   gameContainerDiv.setAttribute("class", "game-container");
+  gameContainerDiv.setAttribute("id", "game-container");
 
   const wordsContainerDiv = document.createElement("div");
   wordsContainerDiv.setAttribute("class", "words-container");
@@ -34,9 +37,44 @@ const constructGameContainer = () => {
   return gameContainerDiv;
 };
 
+const constructGameOverContainer = () => {
+  const gameContainerDiv = document.createElement("div");
+  gameContainerDiv.setAttribute("class", "game-container");
+
+  const wordsContainerDiv = document.createElement("div");
+  wordsContainerDiv.setAttribute("class", "words-container");
+
+  wordsContainerDiv.textContent = "GAME OVER";
+
+  gameContainerDiv.appendChild(wordsContainerDiv);
+
+  return gameContainerDiv;
+};
+
+const startTimer = () => {
+  const timerTick = () => {
+    timerSpanElement.textContent = timerValue;
+    timerValue -= 1;
+
+    if (timerValue < 0) {
+      clearInterval(timer);
+      // construct game over container
+      const gameOverContainer = constructGameOverContainer();
+
+      // remove game container
+      const gamesContainerDiv = document.getElementById("game-container");
+      bodyElement.removeChild(gamesContainerDiv);
+
+      // append game over container to body
+      bodyElement.appendChild(gameOverContainer);
+    }
+  };
+
+  const timer = setInterval(timerTick, 1000);
+};
+
 const startGame = () => {
   // replace the start-game div with the game-container div
-
   // construct the game-container div in JS
   const gameDivElement = constructGameContainer();
 
@@ -45,6 +83,9 @@ const startGame = () => {
 
   // insert the game-container
   bodyElement.appendChild(gameDivElement);
+
+  // start timer here
+  startTimer();
 };
 
 startButtonElement.addEventListener("click", startGame);
